25957d71b60fe5adf15cdd81e471b0c0809447a2
Stepan Muzychka <stepan.muzychka@gmail.com>
2018-06-14 14:06:39 +0200

    create-prefix-tree.sh

--- shell-scripts/create-prefix-tree.sh
+++ shell-scripts/create-prefix-tree.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 pushd ../build
-./prepare-dataset-app -c prefix-tree -t ../corpus --prefix-tree-file ../python/model/prefix-tree
+./typos-corrector ../python/model/dictionary ../python/model/prefix-tree
 popd

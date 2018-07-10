6a3efc0e65dc055367040391326d57873152e325
stepan <stepan.muzychka@gmail.com>
2018-07-10 23:09:56 +0200

    collaborator

--- shell-scripts/build-cplusplus.sh
+++ shell-scripts/build-cplusplus.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+if [ ! -d ../build ]; then
+    mkdir ../build
+fi
+
 pushd ../build
 cmake ../cplusplus -DCMAKE_BUILD_TYPE=RELEASE
 make
---
+++ shell-scripts/collaborator.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# preparations
+apt-get install -y g++ cmake
+
+./build-cplusplus.sh
+./train.sh
+
